1cb403532a62c5de62c6f811edf7e2030bedc38b
WarriusBird <nbgibson@crimson.ua.edu>
2018-07-15 01:23:30 -0400

    refactoring

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MusicPlayer : MonoBehaviour {
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	
+}
